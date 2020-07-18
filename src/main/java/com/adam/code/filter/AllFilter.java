14d2e49ebf7da76a8236352632cd365c50eeb68b
adamboov <48423629+adamboov@users.noreply.github.com>
2020-07-19 00:28:05 +0800

    过滤器编写

---
+++ src/main/java/com/adam/code/filter/AllFilter.java
@@ -0,0 +1,43 @@
+package com.adam.code.filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author VAIO-adam
+ * 过滤器
+ */
+
+@Configuration
+public class AllFilter implements Filter {
+
+    @Bean
+    public FilterRegistrationBean<AllFilter> getAllFilter() {
+
+        FilterRegistrationBean<AllFilter> filter = new FilterRegistrationBean<>();
+
+        filter.setFilter(new AllFilter());
+        filter.addUrlPatterns("/*");
+
+        return filter;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
